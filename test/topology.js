52749e1699909db56cee81a2dafa33055c8eabdf
Mikola Lysenko <mikolalysenko@Mikolas-MacBook-Pro.local>
2015-04-18 19:57:33 -0500

    added topology test

--- test/topology.js
+++ test/topology.js
@@ -2,11 +2,36 @@
 
 var tape = require('tape')
 var vtx = require('../lib/vertex')
-var Graph = require('../lib/graph')
 
 tape('topology', function(t) {
 
-  
+  var verts = []
+  for(var i=0; i<10; ++i) {
+    verts.push(vtx.create(i,i))
+  }
+
+  for(var i=0; i<10; ++i) {
+    var v = verts[i]
+    for(var j=1+i; j<10; j+=(i+1)) {
+      var u = verts[j]
+      vtx.link(u, v)
+
+    }
+  }
+
+  for(var i=0; i<10; ++i) {
+    var v = verts[i]
+    for(var j=1+i; j<10; j+=(i+1)) {
+      var u = verts[j]
+      var v_idx = v.edges.indexOf(u)
+      var u_idx = u.edges.indexOf(v)
+      t.equals(v.edges[v_idx], u)
+      t.equals(u.edges[u_idx], v)
+      var d = Math.abs(v.x - u.x) + Math.abs(v.y - u.y)
+      t.equals(v.lengths[v_idx], d)
+      t.equals(u.lengths[u_idx], d)
+    }
+  }
 
   t.end()
 })
