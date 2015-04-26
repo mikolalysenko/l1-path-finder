4eb8ae89f8bf53ec0c7782cede94638be6aeee04
mikolalysenko <mikolalysenko@gmail.com>
2015-04-25 22:05:53 -0500

    fix output from astar-andrea

--- bench/libraries/astar-andrea.js
+++ bench/libraries/astar-andrea.js
@@ -11,8 +11,8 @@ function preprocess(grid) {
     var path = AStar(array, [sx,sy], [tx,ty])
     if(out) {
       for(var i=0; i<path.length; ++i) {
-        out[2*i] = path[i][1]
-        out[2*i+1] = path[i][0]
+        out[2*i] = path[i][0]
+        out[2*i+1] = path[i][1]
       }
     }
     if(path.length === 0) {
