1c441e7212d19e35116009f7ec871fce217050ac
mikolalysenko <mikolalysenko@gmail.com>
2015-04-28 07:48:57 -0500

    add example

--- example/example.js
+++ example/example.js
@@ -1,2 +1,25 @@
 var ndarray = require('ndarray')
 var createPlanner = require('../lib/planner')
+
+//Create a maze as an ndarray
+var maze = ndarray([
+  0, 1, 0, 0, 0, 0, 0,
+  0, 1, 0, 1, 0, 0, 0,
+  0, 1, 0, 1, 1, 1, 0,
+  0, 1, 0, 1, 0, 0, 0,
+  0, 1, 0, 1, 0, 0, 0,
+  0, 1, 0, 1, 0, 0, 0,
+  0, 1, 0, 1, 0, 1, 1,
+  0, 0, 0, 1, 0, 0, 0,
+], [8, 7])
+
+//Create path planner
+var planner = createPlanner(maze)
+
+//Find path
+var path = []
+var dist = planner.search(0,0,  7,6,  path)
+
+//Log output
+console.log('path length=', dist)
+console.log('path = ', path)
