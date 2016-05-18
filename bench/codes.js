15bdf986afca5b07fafe31b95acb72bc45d709db
Bryce Neal <brycedneal@gmail.com>
2016-05-17 20:01:18 -0700

    Fix easystar.js benchmarks

--- bench/codes.js
+++ bench/codes.js
@@ -6,7 +6,7 @@ var andrea = require('./libraries/astar-andrea')
 
 module.exports = {
   'l1-path-finder': l1path,
-  //'easystar (BROKEN)': easystar,  //Library is too messed up
+  'easystar': easystar,
   'astar-andrea (BROKEN)': andrea,
   'node-pathfinding': nodePathFinding,
   'pathfinding.js: astar': pathfinding.astar,
